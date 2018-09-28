2f2af3bca37d7b83b1191dd3859def728b359b88
Alexander <oz.sasha.ivanov@gmail.com>
2018-09-28 15:31:25 +0300

    Added modal window

---
+++ src/components/BigPhoto.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+export default class BigPhoto extends React.Component {
+  state = {
+    isLoading: false,
+  };
+
+  componentDidMount() {
+    this.loadImage(this.props.url);
+  }
+
+  loadImage = src => {
+    this.setState({ isLoading: true });
+
+    let img = new Image();
+    img.onload = () => {
+      this.setState({ isLoading: false });
+    };
+
+    img.src = src;
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    const { url } = this.props;
+    return isLoading ? <p>Загружаю...</p> : <img src={url} alt="big vk" />;
+  }
+}
+
+BigPhoto.propTypes = {
+  url: PropTypes.string.isRequired,
+};
